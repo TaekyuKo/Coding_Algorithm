15dae886fbdbf10de79ae1c0233542e949b4ffb9
고태규 <taekueko714@gmail.com>
2026-03-31 23:22:21 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2632 ms, Memory: 320596 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] num = new int[n+1][n+1];
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                num[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] sum = new int[n+1][n+1];
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                sum[i][j] = sum[i][j-1]+sum[i-1][j]-sum[i-1][j-1]+num[i][j];
+            }
+        }
+
+        for(int i = 0 ;i<m;i++){
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            System.out.println(sum[x2][y2]-sum[x2][y1-1]-sum[x1-1][y2]+sum[x1-1][y1-1]);
+        }
+    }
+}
